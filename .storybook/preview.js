5861a849e32b226875e800097a7771e25b35ede9
Odailson Silva <odafreelancerti@gmail.com>
2021-07-16 10:32:31 -0300

    save

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,17 @@
 import { addDecorator } from "@storybook/react";
 import GlobalStyles from "../src/styles/global";
 import StoryRouter from "storybook-react-router";
+import { ThemeProvider } from "styled-components";
+import theme from '../src/styles/theme'
 
 addDecorator(StoryRouter());
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   ),
 ];
 
